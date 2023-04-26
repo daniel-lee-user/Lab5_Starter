9178ff5b7d49d08accdf3d56048735655492da6c
Elvis Joa <ejoa@ucsd.edu>
2023-04-25 22:06:01 -0700

    Loaded voices

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,18 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  load_voices();
+}
+
+function load_voices(){
+  window.speechSynthesis.addEventListener("voiceschanged", e => {
+    const voices = speechSynthesis.getVoices();
+    const drop_down_list = document.getElementById("voice-select");
+    for (let i = 0; i < voices.length; i++) {
+      let option = document.createElement("option");
+      option.value = voices[i];
+      option.textContent = voices[i];
+      drop_down_list.add(option);
+    }
+  });
 }

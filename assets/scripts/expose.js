4dce6c7dae1c1ce57441367ee80833733c43efa8
Elvis Joa <ejoa@ucsd.edu>
2023-04-25 21:01:15 -0700

    Added volume icon change

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,21 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  bind_volume();
+}
+
+function bind_volume() {
+  let volume_slider = document.getElementById("volume");
+  volume_slider.addEventListener("change", e => {
+    let volume_icon = document.getElementById("volume-controls").querySelector("img");
+    if (volume_slider.value == 0) {
+      volume_icon.src = "assets/icons/volume-level-0.svg";
+    }else if (volume_slider.value < 33) {
+      volume_icon.src = "assets/icons/volume-level-1.svg";
+    }else if (volume_slider.value < 67) {
+      volume_icon.src = "assets/icons/volume-level-2.svg";
+    }else{
+      volume_icon.src = "assets/icons/volume-level-3.svg";
+    }
+  });
 }

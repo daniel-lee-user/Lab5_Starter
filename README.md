75e07ecc714e61935446bdcd05bcd429636dbeb8
daniel-lee-user <danielhlee02@gmail.com>
2023-04-25 21:12:23 -0700

    Added select horn function

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 5 - Starter
+
+Daniel Lee
+
+Elvis Joa
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,15 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  selectHorn();
 }
+
+function selectHorn() {
+  const horn = document.getElementById("horn-select");
+  horn.addEventListener("change", (event) => {
+    document.getElementsByTagName("img")[0].src = "assets/images/" + event.target.value + ".svg";
+    document.getElementsByClassName("hidden")[0].src = "assets/audio/" + event.target.value + ".mp3";
+  });
+}
+
+

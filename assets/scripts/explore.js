ed4f191f09876f01235f82e26b6fb86e0f86b8d8
daniel-lee-user <danielhlee02@gmail.com>
2023-04-25 22:07:04 -0700

    added presstotalk

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,22 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+var synth = window.speechSynthesis;
 
 function init() {
-  // TODO
+  pressToTalk();
+}
+
+function pressToTalk() {
+  let talkButton = document.querySelector("button");
+  talkButton.addEventListener("click", e => {
+    let textInput = document.getElementById("text-to-speak");
+    let face = document.getElementsByTagName("img")[0];
+    face.src = "assets/images/smiling-open.png";
+    const input = new SpeechSynthesisUtterance(textInput.value);
+    speechSynthesis.speak(input);
+    input.addEventListener("end", (event) => {
+      face.src = "assets/images/smiling.png";
+    });
+  });
 }
